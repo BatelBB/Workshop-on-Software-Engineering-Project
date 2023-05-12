be8e367fa4396110f6ef10aae8a7b89fb6ef5197
Batel Shkolnik <batelshk@post.bgu.ac.il>
2023-05-12 15:38:58 +0300

    Reverted to previouse response

--- src/domain/main/Utils/Response.py
+++ src/domain/main/Utils/Response.py
@@ -1,39 +1,22 @@
-from typing import Generic, TypeVar, Optional
+from typing import TypeVar, Generic
 
-T = TypeVar('T')
+Result = TypeVar('Result')
 
-class Response(Generic[T]):
-    """
-    A class to encapsulate the response of a function or method call, including the result and a description.
-    """
-    def __init__(self, result: Optional[T] = None, description: str = ""):
-        """
-        Constructs a new 'Response' object.
 
-        :param result: The result of the function or method call, defaults to None
-        :param description: A description of the response, defaults to an empty string
-        """
-        self._result = result
-        self._description = description
-        self._success = self._result is not None
+class Response(Generic[Result]):
 
-    @property
-    def result(self) -> Optional[T]:
-        """
-        Returns the result of the function or method call.
-        """
-        return self._result
+    def __init__(self, result: Result, description: str = ""):
+        self.result = result
+        self.description = description
+        # TODO make success a boolean argument instead of using 'result is not None'.
+        if self.result is None:
+            self.success = False
+        elif not self.result:
+            self.success = False
+        else:
+            self.success = True
 
-    @property
-    def description(self) -> str:
-        """
-        Returns the description of the response.
-        """
-        return self._description
 
-    @property
-    def success(self) -> bool:
-        """
-        Returns a boolean indicating whether the function or method call was successful.
-        """
-        return self._success
+    def __str__(self):
+        return f'Request result is: {self.result}\nDescription: {self.description}'
+
