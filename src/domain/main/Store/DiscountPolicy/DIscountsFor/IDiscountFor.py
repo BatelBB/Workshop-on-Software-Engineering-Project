51e834a59e518faf7357eb40c13347d0448052ae
hagai <hagaik@post.bgu.ac.il>
2023-05-27 20:22:42 +0300

    wip

--- src/domain/main/Store/DiscountPolicy/DIscountsFor/IDiscountFor.py
+++ src/domain/main/Store/DiscountPolicy/DIscountsFor/IDiscountFor.py
@@ -3,7 +3,7 @@ from abc import ABC, abstractmethod
 from domain.main.User.Basket import Basket
 
 
-class IDiscountPolicy(ABC):
+class IDiscountFor(ABC):
     @abstractmethod
     def calculate_proce(self, basket: Basket):
         ...
